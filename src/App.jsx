95213e722ba8fd277f543b4b89342ddd09b3de46
denis-springinsfeld <denis.springinsfeld@unilim.fr>
2023-09-05 16:54:46 +0200

    1 file modified, 2 added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import Badge from './components/Badge';
 function App() {
-  return <><p>Hello</p></>;
+  return (
+    <>
+      <Badge label="sold" />
+      <Badge label="out" />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Badge.jsx
@@ -0,0 +1,5 @@
+import './Badge.css';
+
+export default function Badge({ label }) {
+  return <span className="badge">{label}</span>;
+}
